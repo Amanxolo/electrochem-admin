f9d796d6b170b17ae00d7c2571c8a4a6c10cc801
Sagar-6203620715 <sagar6203620715@gmail.com>
2026-04-13 08:53:33 +0530

    minor issue

--- src/app/api/sendEmails/route.ts
+++ src/app/api/sendEmails/route.ts
@@ -262,7 +262,7 @@ export async function POST(req: Request) {
     // 6. Send Email
     const { data: emailResult, error: emailError } = await resend.emails.send({
       from: "sales@electrochembattery.com",
-      to: "sagar9031736538@gmail.com",
+      to: email,
       subject: `Proforma Invoice ${piNumber} - ElectroChem`,
        html: `
           <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
